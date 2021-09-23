0b234da9947f7c8aa3c85e2ab38ddba162c57c66
Maximilian Bösing <2189546+boesing@users.noreply.github.com>
2021-09-23 23:53:05 +0200

    qa: introduce benchmarks
    Signed-off-by: Maximilian Bösing <2189546+boesing@users.noreply.github.com>

---
+++ benchmark/RedisClusterWithIgbinarySerializerStorageAdapterBench.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace LaminasBench\Cache;
+
+use Laminas\Cache\Storage\Adapter\Benchmark\AbstractStorageAdapterBenchmark;
+use LaminasTest\Cache\Storage\Adapter\Laminas\RedisClusterStorageCreationTrait;
+use PhpBench\Benchmark\Metadata\Annotations\Iterations;
+use PhpBench\Benchmark\Metadata\Annotations\Revs;
+use PhpBench\Benchmark\Metadata\Annotations\Warmup;
+use RedisCluster;
+
+/**
+ * @Revs(100)
+ * @Iterations(10)
+ * @Warmup(1)
+ */
+class RedisClusterWithIgbinarySerializerStorageAdapterBench extends AbstractStorageAdapterBenchmark
+{
+    use RedisClusterStorageCreationTrait;
+
+    public function __construct()
+    {
+        parent::__construct($this->createRedisClusterStorage(
+            RedisCluster::SERIALIZER_IGBINARY,
+            false
+        ));
+    }
+}
---
+++ benchmark/RedisStorageAdapterBench.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace LaminasBench\Cache;
+
+use Laminas\Cache\Storage\Adapter\Benchmark\AbstractStorageAdapterBenchmark;
+use LaminasTest\Cache\Storage\Adapter\Laminas\RedisStorageCreationTrait;
+use PhpBench\Benchmark\Metadata\Annotations\Iterations;
+use PhpBench\Benchmark\Metadata\Annotations\Revs;
+use PhpBench\Benchmark\Metadata\Annotations\Warmup;
+use Redis;
+
+/**
+ * @Revs(100)
+ * @Iterations(10)
+ * @Warmup(1)
+ */
+class RedisStorageAdapterBench extends AbstractStorageAdapterBenchmark
+{
+    use RedisStorageCreationTrait;
+
+    public function __construct()
+    {
+        parent::__construct($this->createRedisStorage(
+            Redis::SERIALIZER_NONE,
+            true
+        ));
+    }
+}
